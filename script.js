f85529da58bcdf6f3cbf7ddd639c54177529f5c7
Irina <iro4kaov2015@yandex.ru>
2016-05-14 14:42:45 +0300

    изменен файл script.js

--- script.js
+++ script.js
@@ -6,24 +6,21 @@ $.ajax({
 		console.log(books)
 		var p=0;
 		var i=0;
-		while(i<books.length && p!=4)
-		{
-			$('.row_books').append('<div class="book" id="'+books[i].id+'"><img src="'+books[i].cover.small+'"><div class="book-title">'+books[i].title+'</div></div>');
-			p++;
-			i++;
-			if((i+1)==books.length){
-				$('.button').toggle()
+		booksLoad=function(){
+		 while(i<books.length && p!=4)
+			{
+				$('.row_books').append('<div class="book" id="'+books[i].id+'"><a href="book.html#'+books[i].id+'"><img src="'+books[i].cover.small+'"><div class="book-title">'+books[i].title+'</div></a></div>');
+				p++;
+				i++;
+				if(i==books.length){
+					$('.button').toggle()
+				}
 			}
-		}
+		 }
+		booksLoad();
 		$('.button').on('click',function(){
 			p=0;
-		while(i<books.length && p!=4)
-		{
-			$('.row_books').append('<div class="book" id="'+books[i].id+'"><img src="'+books[i].cover.small+'"><div class="book-title">'+books[i].title+'</div></div>');
-			p++;
-			i++;
-			$('.button').toggle()
-		}
+			booksLoad();
 		})
 
 	}
