d2cacf6fe7a7d65f204ae17f64331c8d7a54f87f
Irina <iro4kaov2015@yandex.ru>
2016-05-14 20:40:28 +0300

    Итоговая сумма

--- order.js
+++ order.js
@@ -4,17 +4,26 @@ $(document).ready(function(){
 		url:'https://netology-fbb-store-api.herokuapp.com/book/'+idBook,
 		success: function(books){
 			$('.forma_book').append('<a href="book.html#'+idBook+'"><img src="'+books.cover.small+'"></a>')
+			$('.forma_book').attr('data-price',books.price);
 			$('a.name_book').append(books.title)
 			$('a.name_book').attr('href','book.html#'+idBook);
-			console.log(books);
-			$('.summ span').append(books.price+'Z')
+			$('.summ span').attr('data-count',$('.forma_book').attr('data-price'));	
+			$('.summ span').text($('.summ span').attr('data-count')+'Z');	
 		}
 	})
 	$.ajax({
 		url:'https://netology-fbb-store-api.herokuapp.com/order/delivery',
 		success:function(delivery){
 			delivery.forEach(function(item,i){
-				$('.delivery').append('<p><input type="radio" name="доставка" value="'+item.name+'">'+item.name+' - '+item.price+'Z'+'</p>')
+				$('.delivery').append('<p><input type="radio" name="доставка" value="'+item.name+'" data-price="'+item.price+'">'+item.name+' - '+item.price+'Z'+'</p>')
+			})
+			$('input:radio[name=доставка]').on('change', function (){
+				priceBook=$('.forma_book').attr('data-price')
+				prise1=parseFloat(priceBook);
+				price2=parseFloat($(this).attr('data-price'))
+				price=prise1+price2;
+				$('.summ span').attr('data-count',price)
+				$('.summ span').text($('.summ span').attr('data-count')+'Z')
 			})
 		}
 	})
