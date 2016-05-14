57e11c80dfe6fdca34753e5a9c422a3152bec209
Irina <iro4kaov2015@yandex.ru>
2016-05-14 15:10:29 +0300

    загрузка информации о книге

---
+++ book.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	idBook= window.location.hash.replace("#","");
+	$.ajax({
+		url:'https://netology-fbb-store-api.herokuapp.com/book/'+idBook,
+		success: function(books){
+			$('.book_about').append('<img src="'+books.cover.large+'">');
+			$('.book_top').append(books.description);
+			for(var i=0;i<books.reviews.length;i++){
+				$('.left').append('<div class="advantages"><img src="'+books.reviews[i].author.pic+'"'+'alt="'+books.reviews[i].author.name+'">«'+books.reviews[i].cite+'»</div>')
+			}
+			for(var i=0;i<books.features.length;i++){
+				$('.right').append('<div class="advantages"><img src="'+books.features[i].pic+'"'+'alt=""'+'>«'+books.features[i].title+'»</div>')
+			}
+			$('.buy span').append(books.price+'Z')
+			console.log(books);
+			
+		}
+	})
+})
