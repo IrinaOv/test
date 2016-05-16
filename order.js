866811c62d66cb81d03bc800b0c0066c4e5c7bdb
Irina <iro4kaov2015@yandex.ru>
2016-05-16 18:03:52 +0300

    добавлена отправка заказа на сервер

--- order.js
+++ order.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
 	idBook= window.location.hash.replace("#","");
+	var name;
+	var currency;
 	$.ajax({
 		url:'https://netology-fbb-store-api.herokuapp.com/book/'+idBook,
 		success: function(books){
 			$('.forma_book').append('<a href="book.html#'+idBook+'"><img src="'+books.cover.small+'"></a>');
 			$('.forma_book').attr('data-price',books.price);
 			$('a.name_book').append(books.title);
+			name=books.title;
 			$('a.name_book').attr('href','book.html#'+idBook);
 			$('.summ span').attr('data-count',$('.forma_book').attr('data-price'));	
 			$('.summ span').text($('.summ span').attr('data-count')+'Z');	
 		}
 	})
-	var d=false;
-	var p=false;
-	var s=false;
+	var flagDelivery=false;
+	var flagPayment=false;
+	var flagInput=false;
 	$.ajax({
 		url:'https://netology-fbb-store-api.herokuapp.com/order/delivery',
 		success:function(delivery){
 			delivery.forEach(function(item,i){
-				$('.delivery').append('<p><input type="radio" name="доставка" value="'+item.name+'" data-price="'+item.price+'"data-address="'+item.needAdress+'">'+item.name+' - '+item.price+'Z'+'</p>')
+				$('.delivery').append('<p><input type="radio" name="доставка" value="'+item.name+'" data-price="'+item.price+'"data-address="'+item.needAdress+'"'+'data-id="'+item.id+'"data-delivery="'+item.currency+'">'+item.name+' - '+item.price+'Z'+'</p>')
 			})
 			$('input:radio[name=доставка]').on('change', function (){
 				priceBook=$('.forma_book').attr('data-price');
@@ -27,7 +30,7 @@ $(document).ready(function(){
 				price=prise1+price2;
 				$('.summ span').attr('data-count',price);
 				$('.summ span').text($('.summ span').attr('data-count')+'Z');
-				d=true;
+				flagDelivery=true;
 				if($(this).attr('data-address')=='true')
 				{
 					$('form .adress').removeClass('disable')
@@ -35,50 +38,77 @@ $(document).ready(function(){
 				else{
 					$('form .adress').addClass('disable')
 				}
+				currency=$('input:radio[name=доставка]:checked').attr('data-delivery');
+				$(".payment").text('Способ оплаты:');
+					$.ajax({
+					url:'https://netology-fbb-store-api.herokuapp.com/order/delivery/'+$('input:radio[name=доставка]:checked').attr('data-id')+'/payment',
+					success:function(payment){
+						payment.forEach(function(item,i){
+							$('.payment').append('<p><input type="radio" name="оплата" value="'+item.title+'"data-id="'+item.id+'">'+item.title+'</p>');
+						});
+						$('input:radio[name=оплата]').on('change', function () {
+							flagPayment=true;
+						});
+					}
+				});
 			})
 		}
 	})
-	$.ajax({
-		url:'https://netology-fbb-store-api.herokuapp.com/order/payment',
-		success:function(payment){
-			payment.forEach(function(item,i){
-				$('.payment').append('<p><input type="radio" name="оплата" value="'+item.title+'">'+item.title+'</p>');
-			});
-			$('input:radio[name=оплата]').on('change', function () {
-				p=true;
-			});
-		}
-	});
+
 	$('button.order').on('click',function(){
 		$('.error_forma').text('');
 		if($('input[name=Имя]').val()==''||$('input[name=Телефон]').val()==''||$('input[name=почта]').val()==''){
 			$('.error_forma').append('<span>Заполните поля Имя, Телефон, Эл.адрес</span>');
-			s=false;
+			flagInput=false;
 		}
 		else{
-			s=true;
+			flagInput=true;
 			if(!$.isNumeric($('input[name=Телефон]').val())||$('input[name=Телефон]').val().length!==11){
 				$('.error_forma').append('<span>Введите номе телефона в формате 79999999999</span>');
-				s=false;
+				flagInput=false;
 			}
 			var post = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
 			if(post.test($('input[name=почта]').val())){
 			}else
 			{
 				$('.error_forma').append('<span>Не верно введен электронный адрес</span>');
-				s=false;
+				flagInput=false;
 			}
 		}
-		if(d==false){
+		if(flagDelivery==false){
 			$('.error_forma').append('<span>Выберете способ доставки</span>');
 		}
-		if(p==false){
+		if(flagPayment==false){
 			$('.error_forma').append('<span>Выберете способ оплаты</span>');
 		}
-		if(s==true && p==true){
-
-			console.log('все верно');
-			
+		if(flagInput==true && flagPayment==true){
+				var post={
+					'manager':'ovi@uvelirsoft.ru',
+					'book':idBook,
+					'name':name,
+					'phone':$('input[name=Телефон]').val(),
+					'email':$('input[name=почта]').val(),
+					'comment':$('textarea[name=Комментарий]').val(),
+					'delivery':{
+						'id':$('input[name=доставка]:checked').attr('data-id'),
+						'address':$('textarea[name=Адрес]').val()
+					},
+					'payment':{
+						'id':$('input[name=оплата]:checked').attr('data-id'),
+						'currency':currency
+					}
+					};
+				$.ajax({
+					type:'POST',
+					url:'https://netology-fbb-store-api.herokuapp.com/order',
+					data:post,
+					success:function(request){
+						console.log('заказ отправлен');
+					},
+					error:  function(){
+						console.log('Возникла ошибка: ');
+          },
+				})	
 		}
 	})
 
